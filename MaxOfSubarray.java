7ee59f18e8bf9ce781322c3cd159588c7bd52c72
HeptaDecane <sooraj1999vs@gmail.com>
2021-07-29 00:09:47 +0530

    Arrays, Queue, Sliding-window

--- MaxOfSubarray.java
+++ MaxOfSubarray.java
@@ -29,8 +29,7 @@ public class MaxOfSubarray {
             int arr[] = new int[n];
 
             //adding all the elements to the array 
-            for(int i = 0; i <n; i++)
-            {
+            for(int i = 0; i <n; i++) {
                 arr[i] =sc.nextInt();
             }
 
