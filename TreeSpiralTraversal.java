b2fade1e55b570dd6233ee604abbe168baeb2548
HeptaDecane <sooraj1999vs@gmail.com>
2021-07-30 12:57:40 +0530

    Tree, Level order traversal

---
+++ TreeSpiralTraversal.java
@@ -0,0 +1,141 @@
+/*
+https://practice.geeksforgeeks.org/problems/level-order-traversal-in-spiral-form/1
+Tags: Tree, Level order traversal
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class TreeSpiralTraversal {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            left=null;
+            right=null;
+        }
+    }
+
+    static Node buildTree(String str){
+
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+
+        inOrder(node.right);
+    }
+
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t=Integer.parseInt(br.readLine());
+
+        while(t-- > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            ArrayList<Integer> result = solve(root);
+            for(int value : result)
+                System.out.print(value + " ");
+            System.out.println();
+
+        }
+    }
+
+    //Function to return a list containing the level order traversal in spiral form.
+    private static ArrayList<Integer> solve(Node root) {
+        // Your code here
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> currentLevel = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if(root == null) return result;
+        queue.add(root);
+
+        boolean reverse = true;
+        while (!queue.isEmpty()){
+
+            int n = queue.size();
+            currentLevel.clear();
+            for(int i=0; i<n; i++){
+                Node node = queue.poll();
+                currentLevel.add(node.data);
+
+                if(node.left != null)
+                    queue.add(node.left);
+
+                if(node.right != null)
+                    queue.add(node.right);
+            }
+            if(reverse)
+                Collections.reverse(currentLevel);
+            result.addAll(currentLevel);
+
+            reverse = !reverse;
+        }
+        return result;
+    }
+}
