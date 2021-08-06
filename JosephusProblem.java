50718ad1ad3ae68821f6deb6ce54a53cf360a172
HeptaDecane <sooraj1999vs@gmail.com>
2021-08-06 21:55:53 +0530

    Bit Magic, Recursion

---
+++ JosephusProblem.java
@@ -0,0 +1,41 @@
+/*
+https://practice.geeksforgeeks.org/problems/josephus-problem/1
+Tags: Bit Magic, Recursion
+*/
+
+import java.util.ArrayList;
+import java.util.Scanner;
+class Recursion {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int T=sc.nextInt();//testcases
+        while(T-->0)
+        {
+            int n,k;
+            //taking input n and k
+            n=sc.nextInt();
+            k=sc.nextInt();
+
+            //calling josephus() function
+            System.out.println(solve(n,k));
+        }
+    }
+
+    public static int solve(int n, int k) {
+        //Your code here
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<n; i++)
+            list.add(i);
+
+        int i = (k-1)%list.size();
+        while (true){
+            if(list.size() == 1) break;
+            else list.remove(i);
+            i = (i+k-1)%list.size();
+        }
+        return list.remove(0)+1;
+    }
+}
+
