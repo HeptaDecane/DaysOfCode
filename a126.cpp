4b0a049fba74bd4009c75723e5be9e444b1a4db9
sooraj.vs <sooraj.vs@bajajfinserv.in>
2022-10-28 00:53:21 +0530

    bugfix

--- a126.cpp
+++ a126.cpp
@@ -7,52 +7,42 @@ class Solution
 public:
     //Function to return the minimum cost to react at bottom
     //right cell from top left cell.
+
     int minimumCostPath(vector<vector<int>>& grid){
         // TODO
+        #define triplet pair<int,pair<int,int>>
+
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> d(n, vector<int>(m,INT_MAX));
-        priority_queue<vector<int>, vector<vector<int>>, Comparator> pq;
+        priority_queue<triplet,vector<triplet>,greater<>>pq;
 
         d[0][0] = grid[0][0];
-        pq.push({0,0,d[0][0]});
+        pq.push({d[0][0],{0,0}});
 
         while (not pq.empty()){
-            auto top = pq.top();
-            int r = top[0];
-            int c = top[1];
-            int distance = top[2];
+            triplet top = pq.top();
+            int distance = top.first;
+
+            auto node = top.second;
+            int r = node.first;
+            int c = node.second;
             pq.pop();
 
             vector<pair<int,int>> deltas = {{0,1},{0,-1},{1,0},{-1,0}};
             for(auto delta : deltas){
                 int next_r = r + delta.first;
                 int next_c = c + delta.second;
-                if(not is_safe(next_r,next_c,grid)) continue;
-
+                if(next_r<0 or next_c<0 or next_r>=n or next_c>=m) continue;
                 int next_distance = grid[next_r][next_c];
+
                 if(d[next_r][next_c] > d[r][c]+next_distance){
                     d[next_r][next_c] = d[r][c]+next_distance;
-                    pq.push({next_r, next_c, next_distance});
+                    pq.push({d[next_r][next_c],{next_r, next_c}});
                 }
             }
         }
         return d[n-1][m-1];
-    }
-
-    // TODO
-    struct Comparator{
-        bool operator()(vector<int> a, vector<int> b){
-            return a[2] > b[2];
-        }
-    };
-
-    // TODO
-    bool is_safe(int i, int j, vector<vector<int>> &grid){
-        int n = grid.size();
-        int m = grid[0].size();
-
-        return i>=0 and j>=0 and i<n and j<m;
     }
 };
 
