6af502dcd0990844bfc22427bd206793923e4827
HeptaDecane <sooraj1999vs@gmail.com>
2022-12-09 01:02:51 +0530

    2022-12-09 01:02:51

--- a143.cpp
+++ a143.cpp
@@ -18,6 +18,16 @@ public:
                 dp[i] = dp[i-1]+1;
         }
         return dp[n];
+
+        int count = 0;
+        while (n>0){
+            if(n%2 == 0)
+                n=n/2;
+            else
+                n = n-1;
+            count++;
+        }
+        return count;
     }
 };
 
