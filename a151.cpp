0f45f570aa3ba6ea12cabafcdb81a74a7ace1052
HeptaDecane <sooraj1999vs@gmail.com>
2023-01-05 23:45:57 +0530

    2023-01-05 23:45:57

--- a151.cpp
+++ a151.cpp
@@ -6,23 +6,21 @@ class Solution{
 public:
     int minJumps(int arr[], int n){
         // TODO
-        if(n<=1) return 0;
-        if(arr[0] == 0) return -1;
+        int max_reach = 0;
+        int steps_left = 0;
+        int jumps = 0;
 
-        int max_reach = arr[0];
-        int steps_left = arr[0];
-        int jumps = 1;
-
-        for(int i=1; i<n; i++){
+        for(int i=0; i<n; i++){
             if(i==n-1) return jumps;
-
             max_reach = max(max_reach, i+arr[i]);
-            steps_left--;
+
             if(steps_left == 0){
                 jumps++;
                 steps_left = max_reach-i;
                 if(steps_left<=0) return -1;
             }
+
+            steps_left--;
         }
 
         return -1;
