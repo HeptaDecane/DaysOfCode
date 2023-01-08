ded4604b7006d525dece925b82ccbd746705890d
HeptaDecane <sooraj1999vs@gmail.com>
2023-01-08 12:23:01 +0530

    2023-01-08 12:23:01

--- a153.cpp
+++ a153.cpp
@@ -5,6 +5,21 @@ class Solution {
 public:
     long long int count(int coins[], int n, int sum) {
         // TODO
+        vector<vector<long long int>> dp(n+1,vector(sum+1,0ll));
+
+        for(int i=0; i<=n; i++) dp[i][0]=1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                int coin = coins[i-1];
+
+                if(coin > j)
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = dp[i][j-coin] + dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
     }
 };
 
